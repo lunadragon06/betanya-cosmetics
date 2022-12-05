72ca1702fc2ea2131bd02e50ed668ca59b6c25ac
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-05 18:29:35 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,14 +1,13 @@
-import displayMessage from "./components/common/displayMessage.js";
-import { saveToken, saveUser } from "./utils/storage.js";
 import { baseUrl } from "./settings/api.js";
 import createMenu from "./components/common/createMenu.js";
+import displayMessage from "./components/common/displayMessage.js";
+import { saveToken, saveUser } from "./utils/storage.js";
 
 const form = document.querySelector("form");
-const username = document.querySelector("#username");
-const password = document.querySelector("#password");
 const message = document.querySelector(".message-container");
+const password = document.querySelector("#password");
 const showPassword = document.querySelector(".checkpass");
-console.log(showPassword);
+const username = document.querySelector("#username");
 
 createMenu();
 
@@ -31,7 +30,6 @@ function submitForm(event) {
 
 async function doLogin(username, password) {
     const url = baseUrl + "auth/local";
-
     const data = JSON.stringify({ identifier: username, password: password });
 
     const options = {
@@ -46,25 +44,22 @@ async function doLogin(username, password) {
         const response = await fetch(url, options);
         const json = await response.json();
 
-        console.log(json);
-
         if (json.user) {
-            // displayMessage("success", "Successfully logged in", ".message-container");
-
             saveToken(json.jwt);
             saveUser(json.user);
-
             location.href = "/products.html";
         }
 
         if (json.error) {
             displayMessage("warning", "Incorrect username and/or password.", ".message-container");
         }
+
     } catch (error) {
         console.log(error);
     }
 }
 
+// Show typed in password on input-click
 showPassword.addEventListener('click', function () {
     if (password.type === "password") {
         password.type = "text";
