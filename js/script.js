691d455f3be16f50a517cfb26ef84826b078de6f
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-05 15:08:58 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,40 +1,41 @@
 import { baseUrl } from "./settings/api.js";
 import createMenu from "./components/common/createMenu.js";
 
-const apiUrl = baseUrl + "products";
-const productsContainer = document.querySelector(".products");
+const api = baseUrl + "products";
+const displayProducts = document.querySelector(".products");
 
 createMenu();
 
+// API call ~ Homepage
 async function getProducts(){
-  const response = await fetch(apiUrl);
+  const response = await fetch(api);
   const results = await response.json();
   
-  document.querySelector(".loading").innerHTML = "";
+  document.querySelector(".loader").innerHTML = "";
 
   results.sort( (a,b) => a.id > b.id ? 1 : -1 ).reverse().forEach(item => {
-    if(!item.featured) {
-      return;
-    }
-    productsContainer.innerHTML += 
-    `<article class="product">
-    <img src="${item.image.url}" alt="${item.category}">
-        <div class="product-card">
-        <div class="product-info">
-        <h2 style="text-transform: capitalize;">${item.brand} ${item.name} ${item.category}</h2>
-            <p class="price-tag">$ ${item.price}</p>
-          </div> 
-            <button class="add-to-cart" data-product="${item.id}" id="to-cart">
-              <i class="fas fa-cart-plus"></i>
-            </button>
-        </div>
-      </article>
+    displayProducts.innerHTML += 
     `
-  })
-  // insert add cart-code here
-
+    <article class="product">
+    <img class="product__img" src="${item.image.url}" alt="${item.category}">
+        <div class="product__card">
+          <h2 class="product__item">
+            ${item.brand} ${item.name} ${item.category}
+          </h2>
+          <p class="product__pricetag">
+            $ ${item.price}
+          </p>
+        </div> 
+        <button class="product__cartbtn" data-product="${item.id}" id="cartbtn">
+          <i class="fas fa-cart-plus"></i>
+        </button>
+      </article>
+    `;
+  });
 }
+
 getProducts();
+
 
 // Header API call  
 async function header() {
@@ -44,25 +45,24 @@ async function header() {
   const response = await fetch(url);
   const json = await response.json();
 
-  console.log(json);
-  console.log(json.img.url);
-
   heroBanner.innerHTML = `
   <section class="header" style="background: url('${json.img.url}') center bottom no-repeat;">
-  <article class="header__content">
-    <h1 class="header__heading" style="color: #E60970;">
+    <article class="header__content">
+      <h1 class="header__heading">
       <span style="color: #0E6500;">
         Shop 
       </span>
         ${json.title}
-    </h1>
-    <h1 class="header__heading" style="color: #0E6500;"> Make Up</h1>
-    <a class="header__ctabtn" href="/products.html">
-      SHOP NOW
-    </a>
-  </article>
-</section>
-`
+      </h1>
+      <h1 class="header__heading" style="color: #0E6500;"> 
+        Make Up
+      </h1>
+      <a class="header__ctabtn" href="/products.html">
+        SHOP NOW
+      </a>
+    </article>
+  </section>
+  `;
 }
 
 header();
