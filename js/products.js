96b2cb78ecaf7b3f8941551749b5cdb714cc6321
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-05 15:08:57 +0100

    Update products.js

--- js/products.js
+++ js/products.js
@@ -4,7 +4,6 @@ import createMenu from "./components/common/createMenu.js";
 import { getToken } from "./utils/storage.js";
 
 const token = getToken();
-
 if (!token) {
     location.href = "/login.html";
 }
@@ -14,38 +13,47 @@ const productsUrl = baseUrl + "products";
 createMenu();
 
 (async function () {
-    const container = document.querySelector(".product-container");
-
+    const productDisplay = document.querySelector(".display__products");
     try {
         const response = await fetch(productsUrl);
         const json = await response.json();
-
-        container.innerHTML = "";
+        productDisplay.innerHTML = "";
 
         json.sort( (a,b) => a.id > b.id ? 1 : -1 ).reverse().forEach(function (product) {
-            container.innerHTML += `<article class="product-item">
-                                    <div class="product-content">
-                                    <img class="product-view" src="${product.image.url}" 
-                                        alt="${product.category}">
-                                    <span class="product-details">
-                                    <h2>${product.name}</h2>
-                                    <div>
-                                    <p><b>Brand name: </b>${product.brand}</p>
-                                    <p style="text-transform: capitalize;"><b style="text-transform: none;">Product type: </b>${product.category}</p>
-                                    </div>
-                                    </span>
-                                    </div>
-                                    <div class="product-action">
-                                    <p style="text-align: right;"><b>$</b> ${product.price}</p>
-                                    <a class="product-link" href="edit.html?id=${product.id}">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                    </div>
-                                    </article>
-                                    `;
-        });
+            productDisplay.innerHTML += 
+            `
+            <article class="productitem">
+                <div class="productitem__content">
+                <img class="productitem__view" src="${product.image.url}" 
+                    alt="${product.category}">
+                    <span class="productitem__details">
+                        <h2>
+                            ${product.name}
+                        </h2>
+                        <div>
+                            <p>
+                                <b>Brand name: </b>
+                                ${product.brand}
+                            </p>
+                            <p style="text-transform: capitalize;">
+                                <b style="text-transform: none;">Product type: </b>${product.category}
+                            </p>
+                        </div>
+                    </span>
+                </div>
+                <div class="productitem__setting">
+                        <p class="productitem__setting-tag" style="text-align: right;">
+                            <b>$</b> ${product.price}
+                        </p>
+                        <a class="productitem__setting-link" href="edit.html?id=${product.id}">
+                            <i class="fas fa-edit"></i>
+                        </a>
+                </div>
+        </article>
+        `;
+    });
     } catch (error) {
         console.log(error);
-        displayMessage("error", error, ".product-container");
+        displayMessage("error", error, ".display__products");
     }
 })();
