947ae959d54f9f9194001b6d23b54b5fc8d79783
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-05 19:52:07 +0100

    Update logoutButton.js

--- js/components/common/logoutButton.js
+++ js/components/common/logoutButton.js
@@ -2,11 +2,9 @@ import { clearStorage } from "../../utils/storage.js";
 
 export default function logoutButton() {
     const button = document.querySelector("#logout");
-
     if (button) {
         button.onclick = function () {
             const doLogout = confirm("You're about to log out. Are you sure you want to proceed this action?");
-
             if (doLogout) {
                 clearStorage();
                 location.href = "/login.html";
