548e502e0e6a317b89814e834b9711635fa1792b
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-11 13:56:39 +0100

    Update createMenu.js

--- js/components/common/createMenu.js
+++ js/components/common/createMenu.js
@@ -6,43 +6,29 @@ export default function createMenu() {
     const navbar = document.querySelector(".menu");
     const username = getUsername();
 
-    let authLink = `<a href="login.html" class="${pathname === "/login.html" ? "active" : ""}">
-                        <i class="fas fa-user"></i>
-                    </a>`;
+    let authLink = `<a href="login.html" class="${pathname === "/login.html" ? "active" : ""}"><i class="fas fa-user"></i></a>`;
+
     if (username) {
-        authLink = `
-                    <a id="logout">
-                        <i class="fas fa-sign-out-alt" id="logout-icon"></i>
-                    </a>
-                    `;
+        authLink = `<a id="logout"><i class="fas fa-sign-out-alt" id="logout-icon"></i></a>`;
     }
-    navbar.innerHTML = `        
-                        <a href="/" class="menu__logo">Betanya Cosmetics</a>
+    
+    navbar.innerHTML = `<a href="/" class="menu__logo">Betanya Cosmetics</a>
                         <!-- Navbar toggler -->
                         <input type="checkbox" id="toggle">
                         <label class="menu__toggler" id="toggler" for="toggle">
                             <span class="menu__toggler-item"><i class="fas fa-bars"></i></span>
                         </label>
-                        <!-- Links -->
                         <ul class="menu__links">
                             <li class="menu__item">
-                                <a href="/" class="${pathname === "/" || pathname === "/index.html" ? "active" : ""}">
-                                    Home
-                                </a>
+                                <a href="/" class="${pathname === "/" || pathname === "/index.html" ? "active" : ""}">Home</a>
                             </li>
                             <li class="menu__item">
-                                <a href="/products.html" class="${pathname === "/products.html" || pathname === "/products.html" ? "active" : ""}">
-                                    Products  
-                                </a>
+                                <a href="/products.html" class="${pathname === "/products.html" || pathname === "/products.html" ? "active" : ""}">Products</a>
                             </li>
                         </ul>
-                        <!-- User section -->
                         <div class="menu__userinterface">
                             ${authLink}
-                            <a href="#" class="${pathname === "#" || pathname === "#" ? "active" : ""}">
-                                <i class="fas fa-shopping-basket"></i>
-                            </a>
-                        </div>
-                    `;
+                            <a href="#" class="${pathname === "#" || pathname === "#" ? "active" : ""}"><i class="fas fa-shopping-basket"></i></a>
+                        </div>`;
     logoutButton();
 }
