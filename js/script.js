f7eff43d099512fce521f96f1294cdcecb935960
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-11 14:00:37 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,37 +9,26 @@ createMenu();
 async function getProducts(){
   const response = await fetch(api);
   const results = await response.json();
-  
   document.querySelector(".loader").innerHTML = "";
 
   results.sort( (a,b) => a.id > b.id ? 1 : -1 ).reverse().forEach(item => {
     displayProducts.innerHTML += 
-    `
-    <article class="product">
+    `<article class="product">
     <img class="product__img" src="${item.image.url}" alt="${item.category}">
         <div class="product__card">
-          <h2 class="product__item">
-            ${item.brand} ${item.name} ${item.category}
-          </h2>
-          <p class="product__pricetag">
-            $ ${item.price}
-          </p>
+          <h2 class="product__item">${item.brand} ${item.name} ${item.category}</h2>
+          <p class="product__pricetag">$ ${item.price}</p>
         </div> 
-        <button class="product__cartbtn" data-product="${item.id}" id="cartbtn">
-          <i class="fas fa-cart-plus"></i>
-        </button>
-      </article>
-    `;
+        <button class="product__cartbtn" data-product="${item.id}" id="cartbtn"><i class="fas fa-cart-plus"></i></button>
+      </article>`;
   });
 }
 
 getProducts();
 
-
 async function header() {
   const heroBanner = document.querySelector("header");
   const url = baseUrl + "header";
-
   const response = await fetch(url);
   const json = await response.json();
 
@@ -47,17 +36,11 @@ async function header() {
   <section class="header" style="background: url('${json.img.url}') center bottom no-repeat;">
     <article class="header__content">
       <h1 class="header__heading">
-      <span style="color: #0E6500;">
-        Shop 
-      </span>
+        <span style="color: #0E6500;">Shop</span>
         ${json.title}
       </h1>
-      <h1 class="header__heading" style="color: #0E6500;"> 
-        Make Up
-      </h1>
-      <a class="header__ctabtn" href="/products.html">
-        SHOP NOW
-      </a>
+      <h1 class="header__heading" style="color: #0E6500;">Make Up</h1>
+      <a class="header__ctabtn" href="/products.html">SHOP NOW</a>
     </article>
   </section>
   `;
