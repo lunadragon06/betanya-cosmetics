9524fa40eecb7ab3a3b64a7baffae234063bf029
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-11 14:08:56 +0100

    Update edit.js

--- js/edit.js
+++ js/edit.js
@@ -14,7 +14,6 @@ createMenu();
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
-
 if (!id) {
     document.location.href = "/";
 }
