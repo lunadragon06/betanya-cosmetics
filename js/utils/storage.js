e7a443906579b1e7a02c75557b98c36b9089b204
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-11 12:59:22 +0100

    Update storage.js

--- js/utils/storage.js
+++ js/utils/storage.js
@@ -4,15 +4,12 @@ const userKey = "user";
 export function saveToken(token) {
     saveToStorage(tokenKey, token);
 }
-
 export function getToken() {
     return getFromStorage(tokenKey);
 }
-
 export function saveUser(user) {
     saveToStorage(userKey, user);
 }
-
 export function getUsername() {
     const user = getFromStorage(userKey);
     if (user) {
@@ -20,15 +17,12 @@ export function getUsername() {
     }
     return null;
 }
-
 export function clearStorage() {
     localStorage.clear();
 }
-
 function saveToStorage(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
 }
-
 function getFromStorage(key) {
     const value = localStorage.getItem(key);
 
