6f800b05300797ab05718f17f081f42bf43c4299
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-11 12:59:16 +0100

    Update deleteButton.js

--- js/components/products/deleteButton.js
+++ js/components/products/deleteButton.js
@@ -3,20 +3,14 @@ import { getToken } from "../../utils/storage.js";
 
 export default function deleteButton(id) {
     const container = document.querySelector(".delete-container");
-
     container.innerHTML = `<button type="button" class="delete" id="delete">Delete</button>`;
-
     const button = document.querySelector("button.delete");
 
     button.onclick = async function () {
-        console.log(id);
-
         const doDelete = confirm("Are you sure you want to delete this?");
-        console.log(doDelete);
 
         if (doDelete) {
             const url = baseUrl + "products/" + id;
-
             const token = getToken();
 
             const options = {
